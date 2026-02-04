f86483ff7b21a9780c088da3fe740a21944ca6e3
RaghadAljuid <s220003258@seu.edu.sa>
2026-02-04 11:46:33 +0300

    Initial Commit

---
+++ Hawlik/HawlikApp.swift
@@ -0,0 +1,17 @@
+//
+//  HawlikApp.swift
+//  Hawlik
+//
+//  Created by Raghad Aljuid on 16/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct HawlikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
