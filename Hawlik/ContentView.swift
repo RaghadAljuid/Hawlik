9d91351e6431fb573636a57c60bf34de5c9d4ebf
RaghadAljuid <s220003258@seu.edu.sa>
2026-02-07 23:02:54 +0300

    حفظ الخريطة

--- Hawlik/ContentView.swift
+++ Hawlik/ContentView.swift
@@ -4,21 +4,18 @@
 //
 //  Created by Raghad Aljuid on 16/08/1447 AH.
 //
-
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 24.7136, longitude: 46.6753),
+        span: MKCoordinateSpan(latitudeDelta: 0.12, longitudeDelta: 0.12)
+    )
 
-#Preview {
-    ContentView()
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .mapStyle(.standard)   // مهم
+            .ignoresSafeArea()
+    }
 }
