c9251c154c1959e680f7716afb2b18fc4943d58a
RaghadAljuid <s220003258@seu.edu.sa>
2026-02-09 12:55:08 +0300

    تعديل الخريطة والتاب

---
+++ Hawlik/AppTapBar.swift
@@ -0,0 +1,18 @@
+//
+//  AppTapBar.swift
+//  Hawlik
+//
+//  Created by Raghad Aljuid on 21/08/1447 AH.
+//
+
+import SwiftUI
+
+struct AppTapBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppTapBar()
+}
