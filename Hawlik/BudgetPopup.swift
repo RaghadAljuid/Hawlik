6ab6f791c0f152d1b23deeb824108646f3f903e5
RaghadAljuid <s220003258@seu.edu.sa>
2026-02-11 13:06:57 +0300

    تحديث التاب بار والبدجت

---
+++ Hawlik/BudgetPopup.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetPopup.swift
+//  Hawlik
+//
+//  Created by Raghad Aljuid on 23/08/1447 AH.
+//
+
+import SwiftUI
+
+struct BudgetPopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BudgetPopup()
+}
