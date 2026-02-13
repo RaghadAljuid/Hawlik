662f8186cd7c7385a286e2e413c65316056ab433
Saba2109 <sabarasheed998@gmail.com>
2026-02-14 01:28:12 +0300

    saved

---
+++ Hawlik/Backto.swift
@@ -0,0 +1,14 @@
+//
+//  Places.swift
+//  Hawlik
+//
+//  Created by saba alrasheed on 24/08/1447 AH.
+//
+
+import SwiftUI
+
+struct Place: Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let image: String
+}
